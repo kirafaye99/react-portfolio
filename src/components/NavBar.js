23f64e0d844e3a7675bd8fae4d72db462929a550
kirafaye99 <kirademarco99@gmail.com>
2021-10-28 22:43:03 -0700

    adding filler to files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function NavBar() {
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
