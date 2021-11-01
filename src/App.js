fe569c438b4e4dbba6725f9208e67ddfb4cb523f
kirafaye99 <kirademarco99@gmail.com>
2021-11-01 15:09:41 -0700

    attempting to fix errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Contact from './components/pages/Contact';
 import Resume from './components/pages/Resume';
-import { Switch, Route } from 'react-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import "tailwindcss/tailwind.css"
 
