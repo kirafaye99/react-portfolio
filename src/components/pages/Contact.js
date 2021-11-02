b9b80017c4f252d3f7661c167c7514b9f2737735
kirafaye99 <kirademarco99@gmail.com>
2021-11-01 18:28:15 -0700

    portfolio page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,43 @@
 
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MailIcon, MenuIcon, PhoneIcon, XIcon } from '@heroicons/react/outline'
+import React from 'react'
+import { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
+import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
 
 
 export default function Contact() {
+
+  const [formState, setFormState] = useState({name:"", email:"", phone:"", subject:"", message:""});
+  const {name, email, phone, subject, message} = formState;
+  const [error, setError] = useState("");
+
+  function handlePageChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (isValid) {
+        setError("Invalid email address");
+      } else {
+        setError("");
+      }
+    } else {
+      if(!e.target.value.length) {
+        setError("This field is required");
+      } else {
+        setError("");
+      }
+    }
+    if (!error) {
+      setFormState({...formState, [e.target.name]: e.target.value});
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!error) {
+      console.log(formState);
+    }
+  }
+
   return (
     <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center bg-white">
       <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 whitespace-normal">{/* Content goes here */}
@@ -32,7 +65,7 @@ export default function Contact() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="relative bg-white shadow-xl">
               <h2 id="contact-heading" className="sr-only">
-                Contact us
+                Contact me
               </h2>
 
               <div className="grid grid-cols-1 lg:grid-cols-3">
@@ -152,46 +185,12 @@ export default function Contact() {
                       <a href="mailto:kirademarco.codes@gmail.com" className="ml-3">kirademarco.codes@gmail.com</a>
                     </dd>
                   </dl>
-                  <ul role="list" className="mt-8 flex space-x-12">
-                    <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
-                        <span className="sr-only">Facebook</span>
-                        <svg className="w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                          <path
-                            fillRule="evenodd"
-                            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
-                        <span className="sr-only">GitHub</span>
-                        <svg className="w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                          <path
-                            fillRule="evenodd"
-                            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a className="text-teal-200 hover:text-teal-100" href="#">
-                        <span className="sr-only">Twitter</span>
-                        <svg className="w-7 h-7" aria-hidden="true" fill="currentColor" viewBox="0 0 24 24">
-                          <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                        </svg>
-                      </a>
-                    </li>
-                  </ul>
                 </div>
 
                 {/* Contact form */}
                 <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-                  <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
-                  <form action="#" method="POST" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                  <h3 className="text-lg font-medium text-warm-gray-900">Send me a message!</h3>
+                  <form action="" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                     <div>
                       <label htmlFor="first-name" className="block text-sm font-medium text-warm-gray-900">
                         First name
@@ -203,6 +202,8 @@ export default function Contact() {
                           id="first-name"
                           autoComplete="given-name"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          defaultValue={name}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -217,6 +218,8 @@ export default function Contact() {
                           id="last-name"
                           autoComplete="family-name"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          defaultValue={name}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -231,6 +234,8 @@ export default function Contact() {
                           type="email"
                           autoComplete="email"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          defaultValue={email}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -251,6 +256,8 @@ export default function Contact() {
                           autoComplete="tel"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
                           aria-describedby="phone-optional"
+                          defaultValue={phone}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -264,6 +271,8 @@ export default function Contact() {
                           name="subject"
                           id="subject"
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border-warm-gray-300 rounded-md"
+                          defaultValue={subject}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -283,7 +292,8 @@ export default function Contact() {
                           rows={4}
                           className="py-3 px-4 block w-full shadow-sm text-warm-gray-900 focus:ring-teal-500 focus:border-teal-500 border border-warm-gray-300 rounded-md"
                           aria-describedby="message-max"
-                          defaultValue={''}
+                          defaultValue={message}
+                          onBlur={handlePageChange}
                         />
                       </div>
                     </div>
@@ -291,6 +301,7 @@ export default function Contact() {
                       <button
                         type="submit"
                         className="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-teal-500 hover:bg-teal-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500 sm:w-auto"
+                        onSubmit={handleSubmit}
                       >
                         Submit
                       </button>
